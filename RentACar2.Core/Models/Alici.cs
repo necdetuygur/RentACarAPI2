94680ead4327122ea09d66377ffa82070daa2b2e
Necdet UYGUR <necdet.uygur@gmail.com>
2021-03-28 16:50:00 +0300

    Entity modeller oluşturuldu.

---
+++ RentACar2.Core/Models/Alici.cs
@@ -0,0 +1,13 @@
+﻿namespace RentACar2.Core.Models
+{
+    public class Alici
+    {
+        public int AliciID { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string TCKimlikNo { get; set; }
+        public string CepTel { get; set; }
+        public string Mail { get; set; }
+        public string Adres { get; set; }
+    }
+}
---
+++ RentACar2.Core/Models/Araba.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar2.Core.Models
+{
+    public class Araba
+    {
+        public int ArabaID { get; set; }
+        public string Plaka { get; set; }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int UretimYili { get; set; }
+        public string YakitTuru { get; set; }
+        public string EhliyetSinifi { get; set; }
+        public int KoltukSayisi { get; set; }
+        public decimal GunlukUcret { get; set; }
+        public int FirmaID { get; set; }
+        public virtual Firma Firma { get; set; }
+    }
+}
